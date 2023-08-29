7699bca96a56687db508c42f50716c1492b45f10
matteoNapoli42 <matteon4242@gmail.com>
2023-08-29 14:36:21 +0200

    initial commit

---
+++ jsnack1/script.js
@@ -0,0 +1,10 @@
+let num1 = prompt("Inserisci il primo numero");
+let num2 = prompt("Inserisci il primo numero");
+
+if(num1>num2)
+    console.log(num1);
+else if (num1 == num2)
+    console.log("I due numeri sono uguali");
+else
+    console.log(num2);
+
