fb6d451f44b92b89a27be6dbc3a461d120e7abcb
matteoNapoli42 <matteon4242@gmail.com>
2023-08-29 14:56:24 +0200

    finished third snack

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,10 +1,10 @@
 let num1 = prompt("Inserisci il primo numero");
 let num2 = prompt("Inserisci il primo numero");
 
-if(num1>num2)
-    console.log(num1);
-else if (num1 == num2)
+if (num1 == num2)
     console.log("I due numeri sono uguali");
+else if(num1>num2)
+    console.log(num1);
 else
     console.log(num2);
 
---
+++ jsnack3/script.js
@@ -0,0 +1,9 @@
+let ris=0;
+
+for(let i=0; i<10; i++)
+{
+    let numero = prompt("Inserisci un numero");
+    ris += parseInt(numero);
+}
+
+console.log(ris);
