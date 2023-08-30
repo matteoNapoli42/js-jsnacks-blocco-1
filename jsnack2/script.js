3d1438481a14ab3e1a5d150aac9cc1e3a04c3208
matteoNapoli42 <matteon4242@gmail.com>
2023-08-30 11:46:41 +0200

    add while cycle

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,3 +1,5 @@
+
+/*Versione aciclica 
 let word1 = prompt("Inserisci la prima parola");
 let word2 = prompt("Inserisci la seconda parola");
 
@@ -13,4 +15,35 @@ else
     console.log(word2);
     console.log(word1);
 }
-    
+*/
+
+/*Versione con ciclo
+
+(con while) Snack 2:
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+let word1;
+let word2;
+let flag=false;
+
+while(flag==false)
+{
+    word1 = prompt("Inserisci la prima parola");
+    word2 =prompt("Inserisci la seconda parola");
+    flag = confirm("Premi OK per stampare o Annulla per reinserire");
+}
+
+if(word1.length<word2.length)
+{
+    console.log(word1);
+    console.log(word2);
+}
+else if(word1.length==word2.length)
+    console.log("Le due parole sono ugualmente lunghe");
+else
+{
+    console.log(word2);
+    console.log(word1);
+}
