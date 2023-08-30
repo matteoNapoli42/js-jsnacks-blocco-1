32d61abe9bb7a5d6eb326d34d6da1f7168f3ba80
matteoNapoli42 <matteon4242@gmail.com>
2023-08-30 10:49:45 +0200

    add while iteraction

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,5 +1,6 @@
-let num1 = prompt("Inserisci il primo numero");
-let num2 = prompt("Inserisci il primo numero");
+/*Versione aciclica 
+
+
 
 if (num1 == num2)
     console.log("I due numeri sono uguali");
@@ -7,4 +8,32 @@ else if(num1>num2)
     console.log(num1);
 else
     console.log(num2);
+*/
+
+/*Versione con ciclo */
+
+/*Da fare con WHILE tutti I prossimi snacks
+Snack 1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+let num1;
+let num2;
+let flag = true;
+while(flag)
+{
+    num1 = prompt("Inserisci il primo numero");
+    num2 = prompt("Inserisci il secondo numero");
+    flag = false;
+}
+
+if (num1 == num2)
+    console.log("I due numeri sono uguali");
+else if(num1>num2)
+    console.log(num1);
+else
+    console.log(num2);
+
+
+
 
